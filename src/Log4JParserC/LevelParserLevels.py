98e208e599d16ec3229ddd8af03b4650f4e9b5f2
Max A. Kiselev <kiselevma@gmail.com>
2015-06-06 17:03:06 +0300

    Refactored LevelParserLevels.py.

--- src/Log4JParserC/LevelParserLevels.py
+++ src/Log4JParserC/LevelParserLevels.py
@@ -1,4 +1,4 @@
-﻿z = [
+﻿log4JLevels = [
     ('ALERT', '100000'),
     ('ALL', 'INT_MIN'),
     ('CRITICAL', '90000'),
@@ -19,68 +19,75 @@
     #('log4net:DEBUG', '120000')
     ]
 
-class TN (object):
-    def __init__ (self):
-        self.V = -1;
-        self.C = [None for x in range (26)];
+class PrefixTreeNode(object):
+    def __init__(self):
+        self.Value = -1;
+        self.Children = [None for _ in range(26)];
 
-def add (t, z):
-    (n, v) = z
-    if len (n) == 0:
-        t.V = v
+    def add(self, name, value):
+        if len(name) == 0:
+            self.Value = value
+        else:
+            i = ord(name[0]) - ord ('A')
+            self.Children[i] = PrefixTreeNode()
+            self.Children[i].add(name[1:], value)
+
+root = PrefixTreeNode()
+for (name, value) in log4JLevels:
+    root.add(name, value)
+
+def node_c_id(parent_node_name, child_node_index = None):
+    if child_node_index is None:
+        node_name = parent_node_name
     else:
-        i = ord (n[0]) - ord ('A')
-        t.C[i] = TN ()
-        add (t.C[i], (n[1:], v))
-
-r = TN ()
-for x in z: add (r, x)
-
-def node_name (rp, i = None):
-    if i is None:
-        p = rp
+        ch = chr(child_node_index + ord('A'))
+        node_name = parent_node_name + ch
+    if node_name != '':
+        result ='Level{}_'.format(node_name.title())
     else:
-        ch = chr(i + ord ('A'))
-        p = rp + ch
-    suf = p.title () if p != '' else 's'
-    return 'Level{}_'.format (suf)
+        result = 'Levels_'
+    return result
 
-def tree_node_ref (r, rp, i):
-    c = r.C[i]
-    return 'NULL' if r.C[i] is None else '&{}'.format (node_name (rp, i))
+def tree_node_ref(tree_node, node_name, child_node_index):
+    if tree_node.Children[child_node_index] is None:
+        result = 'NULL'
+    else:
+        c_id = node_c_id (node_name, child_node_index)
+        result = '&{}'.format(c_id)
+    return result
 
-def tree_node_name (r, rp, i):
-    c = r.C[i]
-    ch = chr(i + ord ('A'))
-    l = len (tree_node_ref (r, rp, i))
-    return ch.rjust (l)
+def tree_node_slot_name(tree_node, node_name, child_node_index):
+    ch = chr(child_node_index + ord('A'))
+    child_ref = tree_node_ref(tree_node, node_name, child_node_index)
+    l = len(child_ref)
+    return ch.rjust(l)
 
-def print_tree (t, p = ''):
-    if t is None:
+def print_tree(tree_node, node_name = ''):
+    if tree_node is None:
         raise 't is None'
     
-    for i in range (26):
-        c = t.C[i]
-        if c is not None:
-            ch = chr(i + ord ('A'))
-            cp = p + ch
-            print_tree (c, cp)
-    print ('static const LevelTreeNode_ {} ='.format (node_name (p)))
-    print ('{')
+    for i in range(len(tree_node.Children)):
+        child = tree_node.Children[i]
+        if child is not None:
+            ch = chr(i + ord('A'))
+            childId = node_name + ch
+            print_tree(child, childId)
+    print('static const LevelTreeNode_ {} ='.format(node_c_id(node_name)))
+    print('{')
     
-    print ('    {},'.format (t.V))
+    print('    {},'.format(tree_node.Value))
+
+    slot_names = [tree_node_slot_name(tree_node, node_name, i) for i in range(len(tree_node.Children))]
+    childrefs = [tree_node_ref(tree_node, node_name, i) for i in range(len(tree_node.Children))]
+    print('    //{}'.format('  '.join(slot_names)))
+    print('    {{ {} }}'.format(', '.join(childrefs)))
     
-    childref_comment = '  '.join ([tree_node_name (t, p, i) for i in range (26)])
-    print ('    //{}'.format (childref_comment))
-    childrefs = ', '.join ([tree_node_ref (t, p, i) for i in range (26)])
-    print ('    {{ {} }}'.format (childrefs))
-    
-    print ('};')
+    print('};')
 
-print ('#pragma region Levels definition')
-print ()
+print('#pragma region Levels definition')
+print()
 
-print_tree (r)
+print_tree(root)
 
-print ()
-print ('#pragma endregion')
+print()
+print('#pragma endregion')
