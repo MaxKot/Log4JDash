712509448d1b32d35994c10ae4ed0af15793c523
Max A. Kiselev <kiselevma@gmail.com>
2015-10-09 22:26:59 +0300

    Moved dependency on the sample file in the tests.

--- src/Log4JParserNet.Tests/FilterLoggerTests.cs
+++ src/Log4JParserNet.Tests/FilterLoggerTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using NUnit.Framework;
 
 namespace Log4JParserNet.Tests
@@ -6,6 +7,16 @@ namespace Log4JParserNet.Tests
     [TestFixture]
     public class FilterLoggerTests
     {
+        private const string Sample = @"<?xml version=""1.0"" encoding=""windows-1251""?>
+<log4j:event logger=""Root.ChildA.LoggerA2"" timestamp=""1411231353782"" level=""INFO"" thread=""Thread-1""><log4j:message>#1. Test event A.</log4j:message><log4j:properties><log4j:data name=""log4jmachinename"" value=""EXAMPLE_PC"" /><log4j:data name=""log4japp"" value=""LogGenerator.exe"" /><log4j:data name=""log4net:Identity"" value="""" /><log4j:data name=""log4net:UserName"" value=""EXAMPLE_PC\Dev"" /><log4j:data name=""log4net:HostName"" value=""EXAMPLE_PC"" /></log4j:properties></log4j:event>
+<log4j:event logger=""Root.ChildB.LoggerB2"" timestamp=""1411231353792"" level=""DEBUG"" thread=""Thread-2""><log4j:message>#2. Test event B.</log4j:message><log4j:properties><log4j:data name=""log4jmachinename"" value=""EXAMPLE_PC"" /><log4j:data name=""log4japp"" value=""LogGenerator.exe"" /><log4j:data name=""log4net:Identity"" value="""" /><log4j:data name=""log4net:UserName"" value=""EXAMPLE_PC\Dev"" /><log4j:data name=""log4net:HostName"" value=""EXAMPLE_PC"" /></log4j:properties></log4j:event>
+<log4j:event logger=""Root.ChildA.LoggerA2"" timestamp=""1411231353792"" level=""FATAL"" thread=""Thread-3""><log4j:message>#3. Test event C. С кирилицей.</log4j:message><log4j:properties><log4j:data name=""log4jmachinename"" value=""EXAMPLE_PC"" /><log4j:data name=""log4japp"" value=""LogGenerator.exe"" /><log4j:data name=""log4net:Identity"" value="""" /><log4j:data name=""log4net:UserName"" value=""EXAMPLE_PC\Dev"" /><log4j:data name=""log4net:HostName"" value=""EXAMPLE_PC"" /></log4j:properties></log4j:event>
+<log4j:event logger=""Root.ChildA.LoggerA1"" timestamp=""1411231353793"" level=""WARN"" thread=""Thread-4""><log4j:message>#4. Test event E.</log4j:message><log4j:properties><log4j:data name=""log4jmachinename"" value=""EXAMPLE_PC"" /><log4j:data name=""log4japp"" value=""LogGenerator.exe"" /><log4j:data name=""log4net:Identity"" value="""" /><log4j:data name=""log4net:UserName"" value=""EXAMPLE_PC\Dev"" /><log4j:data name=""log4net:HostName"" value=""EXAMPLE_PC"" /></log4j:properties></log4j:event>
+<log4j:event logger=""Root.ChildA.LoggerA1"" timestamp=""1411231353795"" level=""ERROR"" thread=""Thread-5""><log4j:message>#5. Test event F.</log4j:message><log4j:properties><log4j:data name=""log4jmachinename"" value=""EXAMPLE_PC"" /><log4j:data name=""log4japp"" value=""LogGenerator.exe"" /><log4j:data name=""log4net:Identity"" value="""" /><log4j:data name=""log4net:UserName"" value=""EXAMPLE_PC\Dev"" /><log4j:data name=""log4net:HostName"" value=""EXAMPLE_PC"" /></log4j:properties></log4j:event>
+";
+
+        private static readonly byte[] sampleBytes = Encoding.GetEncoding (1251).GetBytes (Sample);
+
         [Test]
         public void FiltersEventsWithLoggerStartingWithPrefix ()
         {
@@ -53,7 +64,8 @@ namespace Log4JParserNet.Tests
                 }
             };
 
-            using (var source = Log4JFile.Create ("sample-1.xml"))
+            using (var sourceStream = new MemoryStream (sampleBytes))
+            using (var source = Log4JFile.Create (sourceStream))
             using (var subject = new FilterLogger ("Root.ChildA"))
             {
                 source.Encoding = Encoding.GetEncoding (1251);
@@ -89,7 +101,8 @@ namespace Log4JParserNet.Tests
                 }
             };
 
-            using (var source = Log4JFile.Create ("sample-1.xml"))
+            using (var sourceStream = new MemoryStream (sampleBytes))
+            using (var source = Log4JFile.Create (sourceStream))
             using (var subject = new FilterLogger ("Root.ChildA.LoggerA1"))
             {
                 source.Encoding = Encoding.GetEncoding (1251);
--- src/Log4JParserNet.Tests/FilterTimestampTests.cs
+++ src/Log4JParserNet.Tests/FilterTimestampTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using NUnit.Framework;
 
@@ -7,6 +8,16 @@ namespace Log4JParserNet.Tests
     [TestFixture]
     public class FilterTimestampTests
     {
+        private const string Sample = @"<?xml version=""1.0"" encoding=""windows-1251""?>
+<log4j:event logger=""Root.ChildA.LoggerA2"" timestamp=""1411231353782"" level=""INFO"" thread=""Thread-1""><log4j:message>#1. Test event A.</log4j:message><log4j:properties><log4j:data name=""log4jmachinename"" value=""EXAMPLE_PC"" /><log4j:data name=""log4japp"" value=""LogGenerator.exe"" /><log4j:data name=""log4net:Identity"" value="""" /><log4j:data name=""log4net:UserName"" value=""EXAMPLE_PC\Dev"" /><log4j:data name=""log4net:HostName"" value=""EXAMPLE_PC"" /></log4j:properties></log4j:event>
+<log4j:event logger=""Root.ChildB.LoggerB2"" timestamp=""1411231353792"" level=""DEBUG"" thread=""Thread-2""><log4j:message>#2. Test event B.</log4j:message><log4j:properties><log4j:data name=""log4jmachinename"" value=""EXAMPLE_PC"" /><log4j:data name=""log4japp"" value=""LogGenerator.exe"" /><log4j:data name=""log4net:Identity"" value="""" /><log4j:data name=""log4net:UserName"" value=""EXAMPLE_PC\Dev"" /><log4j:data name=""log4net:HostName"" value=""EXAMPLE_PC"" /></log4j:properties></log4j:event>
+<log4j:event logger=""Root.ChildA.LoggerA2"" timestamp=""1411231353792"" level=""FATAL"" thread=""Thread-3""><log4j:message>#3. Test event C. С кирилицей.</log4j:message><log4j:properties><log4j:data name=""log4jmachinename"" value=""EXAMPLE_PC"" /><log4j:data name=""log4japp"" value=""LogGenerator.exe"" /><log4j:data name=""log4net:Identity"" value="""" /><log4j:data name=""log4net:UserName"" value=""EXAMPLE_PC\Dev"" /><log4j:data name=""log4net:HostName"" value=""EXAMPLE_PC"" /></log4j:properties></log4j:event>
+<log4j:event logger=""Root.ChildA.LoggerA1"" timestamp=""1411231353793"" level=""WARN"" thread=""Thread-4""><log4j:message>#4. Test event E.</log4j:message><log4j:properties><log4j:data name=""log4jmachinename"" value=""EXAMPLE_PC"" /><log4j:data name=""log4japp"" value=""LogGenerator.exe"" /><log4j:data name=""log4net:Identity"" value="""" /><log4j:data name=""log4net:UserName"" value=""EXAMPLE_PC\Dev"" /><log4j:data name=""log4net:HostName"" value=""EXAMPLE_PC"" /></log4j:properties></log4j:event>
+<log4j:event logger=""Root.ChildA.LoggerA1"" timestamp=""1411231353795"" level=""ERROR"" thread=""Thread-5""><log4j:message>#5. Test event F.</log4j:message><log4j:properties><log4j:data name=""log4jmachinename"" value=""EXAMPLE_PC"" /><log4j:data name=""log4japp"" value=""LogGenerator.exe"" /><log4j:data name=""log4net:Identity"" value="""" /><log4j:data name=""log4net:UserName"" value=""EXAMPLE_PC\Dev"" /><log4j:data name=""log4net:HostName"" value=""EXAMPLE_PC"" /></log4j:properties></log4j:event>
+";
+
+        private static readonly byte[] sampleBytes = Encoding.GetEncoding (1251).GetBytes (Sample);
+
         [Test]
         public void FiltersEventsWithMinTimestamp ()
         {
@@ -54,7 +65,8 @@ namespace Log4JParserNet.Tests
                 }
             };
 
-            using (var source = Log4JFile.Create ("sample-1.xml"))
+            using (var sourceStream = new MemoryStream (sampleBytes))
+            using (var source = Log4JFile.Create (sourceStream))
             using (var subject = new FilterTimestamp (1411231353792L, Int64.MaxValue))
             {
                 source.Encoding = Encoding.GetEncoding (1251);
@@ -100,7 +112,8 @@ namespace Log4JParserNet.Tests
                 }
             };
 
-            using (var source = Log4JFile.Create ("sample-1.xml"))
+            using (var sourceStream = new MemoryStream (sampleBytes))
+            using (var source = Log4JFile.Create (sourceStream))
             using (var subject = new FilterTimestamp (Int64.MinValue, 1411231353792L))
             {
                 source.Encoding = Encoding.GetEncoding (1251);
@@ -146,7 +159,8 @@ namespace Log4JParserNet.Tests
                 }
             };
 
-            using (var source = Log4JFile.Create ("sample-1.xml"))
+            using (var sourceStream = new MemoryStream (sampleBytes))
+            using (var source = Log4JFile.Create (sourceStream))
             using (var subject = new FilterTimestamp (1411231353792L, 1411231353793L))
             {
                 source.Encoding = Encoding.GetEncoding (1251);
@@ -172,7 +186,8 @@ namespace Log4JParserNet.Tests
                 }
             };
 
-            using (var source = Log4JFile.Create ("sample-1.xml"))
+            using (var sourceStream = new MemoryStream (sampleBytes))
+            using (var source = Log4JFile.Create (sourceStream))
             using (var subject = new FilterTimestamp (1411231353793L, 1411231353793L))
             {
                 source.Encoding = Encoding.GetEncoding (1251);
