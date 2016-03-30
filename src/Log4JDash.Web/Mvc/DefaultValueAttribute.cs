4f075003c43581c79a14eeeb35c5abfcea5f592f
Max A. Kiselev <kiselevma@gmail.com>
2016-03-30 22:47:24 +0300

    Implemented an attribute to specify a default value for a property.

---
+++ src/Log4JDash.Web/Mvc/DefaultValueAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace Log4JDash.Web.Mvc
+{
+    internal sealed class DefaultValueAttribute : MetadataAttributeBase
+    {
+        private readonly object value_;
+
+        public DefaultValueAttribute (object value)
+        {
+            value_ = value;
+        }
+
+        internal override void GetMetadataForProperty
+            (Func<object> modelAccessor, Type containerType, PropertyDescriptor propertyDescriptor, ModelMetadata result)
+        {
+            result.AdditionalValues.Add (DefaultValueUtil.MetadataKey, value_);
+        }
+    }
+}
---
+++ src/Log4JDash.Web/Mvc/DefaultValueUtil.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Log4JDash.Web.Mvc
+{
+    public static class DefaultValueUtil
+    {
+        public const string MetadataKey = "DefaultValueUtil.Value";
+
+        public static void PopulateHtmlAttributes (ModelMetadata modelMetadata, IDictionary<string, object> htmlAttributes)
+        {
+            object value;
+            if (modelMetadata.AdditionalValues.TryGetValue (MetadataKey, out value))
+            {
+                htmlAttributes.Add ("data-default", value);
+            }
+        }
+    }
+}
